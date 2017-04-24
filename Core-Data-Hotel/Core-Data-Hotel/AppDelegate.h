9f425b52588defd9c37dd43c26b338c0e4f4168d
Pavel Parkhomey <pavelpark@aol.com>
2017-04-24 09:02:11 -0700

    Set up the GitIgnore and created a new branch.

---
+++ Core-Data-Hotel/Core-Data-Hotel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Core-Data-Hotel
+//
+//  Created by Pavel Parkhomey on 4/24/17.
+//  Copyright © 2017 Pavel Parkhomey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
