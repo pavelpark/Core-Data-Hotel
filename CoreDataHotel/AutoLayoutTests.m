74c742c4e4dcdf2dc305a2ec375f8502269e54da
Pavel Parkhomey <pavelpark@aol.com>
2017-04-26 10:55:51 -0700

    created a UnitTest file

---
+++ CoreDataHotel/AutoLayoutTests.m
@@ -0,0 +1,40 @@
+//
+//  AutoLayoutTests.m
+//  CoreDataHotel
+//
+//  Created by Pavel Parkhomey on 4/26/17.
+//  Copyright © 2017 Pavel Parkhomey. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "AutoLayout.h"
+
+@interface AutoLayoutTests : XCTestCase
+
+@property(strong, nonatomic) UIViewController *testController;
+
+@property(strong,nonatomic) UIView *testView1;
+@property(strong,nonatomic) UIView *testView2;
+
+@end
+
+@implementation AutoLayoutTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    self.testController = [[UIViewController alloc]init];
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    self.testController = nil;
+    
+    [super tearDown];
+}
+
+-(void)testViewControllerNotNil{
+    XCTAssertNotNil(self.testController, @"The testController is nil!");
+}
+
+@end
